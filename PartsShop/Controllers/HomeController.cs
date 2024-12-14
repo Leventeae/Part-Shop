f49bf66b3448fa5e12ddd281622cd42abf4c4db2
Levente Kovács <kovacslevi000@gmail.com>
2024-12-14 20:06:28 +0100

    Qol Updates

--- PartsShop/Controllers/HomeController.cs
+++ PartsShop/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace PartsShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
     }
 }
